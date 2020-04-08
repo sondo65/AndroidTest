acc2a58968311a948b186761fb40c1a265c5a0b7
sondo65 <sondo65@gmail.com>
2020-04-08 19:18:31 +0700

    refactor code

--- app/src/main/java/sondo65/com/androidtest/utils/Constants.java
+++ app/src/main/java/sondo65/com/androidtest/utils/Constants.java
@@ -3,6 +3,6 @@ package sondo65.com.androidtest.utils;
 public class Constants {
 
     //If you want to change DEFINED_ROW value.Clear data or reinstall app to see update!
-    public static final int DEFINED_ROW = 272128;
+    public static final double DEFINED_ROW = 272128;
     public static final int LOAD_MORE_ITEM = 30;
 }
